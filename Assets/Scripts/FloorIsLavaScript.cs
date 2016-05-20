c1ac2c992886f42dd26403bf7215fd42536dc8b1
CoolTaff12 <cool_taff@yahoo.com>
2016-05-20 12:54:37 +0200

    New Music, New Gym and FloorIsLavaScript

---
+++ Assets/Scripts/FloorIsLavaScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorIsLavaScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        //If the object is the player
+        if (other.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>())
+        {
+            GameObject target = other.gameObject;
+            target.GetComponent<PlayerInfo>().Cmd_KillYourself(target);
+        }
+    }
+}
