8188c6baf260a632eb72f5fbe540d8137333aa88
Ronald Mallengren <ronald.mallengren@gmail.com>
2016-05-25 16:32:53 +0200

    New Push to fix shades

--- Assets/Scripts/RotateTheRings.cs
+++ Assets/Scripts/RotateTheRings.cs
@@ -6,11 +6,8 @@ public class RotateTheRings : MonoBehaviour
 {
     public float RotateSpeed;
 
-    /// <summary>
-    /// This rotates the object
-    /// </summary>
-    // Update is called once per frame
-    void Update ()
+	// Update is called once per frame
+	void Update ()
     {
         transform.Rotate(new Vector3(0f, RotateSpeed, 0.0f));
     }
