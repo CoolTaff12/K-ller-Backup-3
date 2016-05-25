08a55848edcf2d41ac01deb49aa68a080eab9d90
CoolTaff12 <cool_taff@yahoo.com>
2016-05-25 16:39:38 +0200

    Revert "New Push to fix shades"
    This reverts commit 8188c6baf260a632eb72f5fbe540d8137333aa88.

--- Assets/Scripts/RotateTheRings.cs
+++ Assets/Scripts/RotateTheRings.cs
@@ -6,8 +6,11 @@ public class RotateTheRings : MonoBehaviour
 {
     public float RotateSpeed;
 
-	// Update is called once per frame
-	void Update ()
+    /// <summary>
+    /// This rotates the object
+    /// </summary>
+    // Update is called once per frame
+    void Update ()
     {
         transform.Rotate(new Vector3(0f, RotateSpeed, 0.0f));
     }
